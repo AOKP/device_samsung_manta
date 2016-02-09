ee9258e7897a989b838dd9904d8bfbefd90b3c63
Dirk Rettschlag <dirk.rettschlag@gmail.com>
2016-02-09 20:52:07 +0100

    AOKP-fy
    Change-Id: Ib685ecf7a46f74fbcab2e439602ca065064db177
    Signed-off-by: Dirk Rettschlag <dirk.rettschlag@gmail.com>

--- aokp.mk
+++ aokp.mk
@@ -7,17 +7,17 @@ TARGET_SCREEN_WIDTH := 2560
 PRODUCT_RELEASE_NAME := Nexus10
 
 # Inherit some common CM stuff.
-$(call inherit-product, vendor/cm/config/common_full_tablet_wifionly.mk)
+$(call inherit-product, vendor/aokp/configs/common_full_tablet_wifionly.mk)
 
 # Enhanced NFC
-$(call inherit-product, vendor/cm/config/nfc_enhanced.mk)
+$(call inherit-product, vendor/aokp/configs/nfc_enhanced.mk)
 
 # Inherit device configuration
 $(call inherit-product, device/samsung/manta/full_manta.mk)
 
 ## Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := manta
-PRODUCT_NAME := cm_manta
+PRODUCT_NAME := aokp_manta
 PRODUCT_BRAND := Google
 PRODUCT_MODEL := Nexus 10
 PRODUCT_MANUFACTURER := samsung
